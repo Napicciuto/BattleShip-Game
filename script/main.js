ba2ca0989dba6f94a603f74b1995b91a62a6c684
napicciuto <nick@gmdx.com>
2013-08-04 19:32:42 -0400

    Added computer to game

--- script/main.js
+++ script/main.js
@@ -7,7 +7,6 @@
 *
 *****************************************************************************************/
 
-
 var battle_box = {
 	ships : new Array(
 		 {
@@ -15,6 +14,7 @@ var battle_box = {
 			sunk : false,
 			cell_count : 3,
 			cell : new Array(),
+			cell_computer : new Array(),
 			hit: function() {
 				this.cell_count--;
 				if(this.cell_count <= 0){
@@ -30,6 +30,7 @@ var battle_box = {
 			sunk : false,
 			cell_count : 2,
 			cell : new Array(),
+			cell_computer : new Array(),
 			hit: function() {
 				this.cell_count--;
 				if(this.cell_count <= 0){
@@ -63,41 +64,8 @@ function safe_random_number(num1, num2 = 1) {
 	return Math.round(Math.random() * (1 , (num1-1)))+num2;
 }
 
-// load battle boats
-for(i in battle_box.ships){
-	var plane = safe_random_number(2,0);
-	var count = battle_box.ships[i].cell_count;
-	var start_number = safe_random_number(10 - count);
-	var cells = new Array();
-	var xx = 0;
-	var yy = 0;	
-	
-	
-	for(j = 1; j <= count; j++){ 
-		// horizontal or vertical
-		if(plane){		
-			if(j == 1){
-				xx = safe_random_number(10);
-			}	
-			yy = start_number;
-			cell = { x : xx, y : (yy+j) };
-			console.log("x : "+xx+" - y "+(yy+j));
-		}else{	
-			if(j == 1){
-				yy = safe_random_number(10);
-			}			
-			xx = start_number;
-			cell = { x : (xx+j), y : yy };	
-			console.log("x : "+(xx+j)+" - y "+yy);
-		}
-	
-		battle_box.ships[i].cell.push(cell);
-	
-	}
-	
 
 
-}
 // make sure ships dont overlap
 
 battle_box.gameover.count =  battle_box.ships.length ;
@@ -123,30 +91,76 @@ var case_test = {
 // Make GUI grid
 function makeGrid(v){ 
 	var e = document.body; // whatever you want to append the rows to: 
-  	for(var i = 1; i < v; i++){ 
-    	var row = document.createElement("div"); 
-    	row.className = "rows"; 
-    	for(var x = 1; x <= v; x++){ 
-        	var cell = document.createElement("div"); 
-        	cell.className = "battleBlock"; 
-        	cell.innerHTML = "<div class='xAlis axis'>"+i+"</div><div class='yAxis axis'>"+x+"</div><div class='active axis'>true</div>";
-        	row.appendChild(cell); 
-    	} 
-    	e.appendChild(row); 
-  	} 
+	for(var j = 1; j <= 2; j++){
+		var grid = document.createElement("div"); 
+		grid.id = "grid"+j;
+    	grid.className = "grid";
+
+		for(var i = 1; i <= v; i++){ 
+	    	var row = document.createElement("div"); 
+	    	row.className = "rows"; 
+	    	for(var x = 1; x <= v; x++){ 
+	        	var cell = document.createElement("div"); 
+	        	cell.className = "battleBlock"; 
+	        	cell.innerHTML = "<div class='xAlis axis'>"+i+"</div><div class='yAxis axis'>"+x+"</div><div class='active axis'>true</div>";
+	        	row.appendChild(cell); 
+	    	} 
+	    	grid.appendChild(row); 
+	  	}
+	e.appendChild(grid); 
+	}
+	
+  	// load battle boats
+	for(var k = 0; k < 2; k++) {
+		for(i in battle_box.ships){
+			var plane = safe_random_number(2,0);
+			var count = battle_box.ships[i].cell_count;
+			var start_number = safe_random_number(10 - count);
+		 	var xx = 0;
+			var yy = 0;	
+
+			for(j = 1; j <= count; j++){ 
+				// horizontal or vertical
+				if(plane){		
+					if(j == 1){
+						xx = safe_random_number(10);
+					}	
+					yy = start_number + j;
+					cell = { x : xx, y : yy };
+					//console.log("x : "+xx+" - y "+(yy+j));
+				}else{	
+					if(j == 1){
+						yy = safe_random_number(10);
+					}			
+					xx = start_number + j;
+					cell = { x : xx, y : yy };	
+					//console.log("x : "+(xx+j)+" - y "+yy);
+				}
+
+				if(k){
+					battle_box.ships[i].cell_computer.push(cell);
+					var square = document.getElementById("grid1");
+					square.childNodes[xx].childNodes[yy].style.backgroundColor = "green";
+				} else {
+					battle_box.ships[i].cell.push(cell);
+				}
+			}
+		}
+	}
+	
 }
 
 
 
 // Game controller
 $(function(){
-	$("body").delegate('.battleBlock', 'click', function(ev){
+	$("body").delegate('#grid2 .battleBlock', 'click', function(ev){
 		var square = $(this);
 		var axis = ev.target;
 		var x = axis.childNodes[0].innerHTML;
 		var y = axis.childNodes[1].innerHTML;
 		var active = axis.childNodes[2];
-		
+				
 		if(active.innerHTML === 'true' ) {
 			if(case_test.test(x,y)){
 				square.css('background', 'red');
