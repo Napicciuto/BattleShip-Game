a00698ae99dd0a59436b2dccbde5ddacd0edfa9d
napicciuto <nick@gmdx.com>
2013-08-04 21:40:51 -0400

    code clean up

--- script/main.js
+++ script/main.js
@@ -19,7 +19,7 @@ var battle_box = {
 				this.cell_count--;
 				if(this.cell_count <= 0){
 					this.sunk = true;
-					console.log("you sunk my "+this.name);
+					alert("you sunk my "+this.name);
 					battle_box.gameover.is_gameover();
 				}
 			}
@@ -35,7 +35,7 @@ var battle_box = {
 				this.cell_count--;
 				if(this.cell_count <= 0){
 					this.sunk = true;
-					console.log("you sunk my "+this.name);
+					alert("you sunk my "+this.name);
 					battle_box.gameover.is_gameover();
 				}
 			}
@@ -49,7 +49,7 @@ var battle_box = {
 				if(battle_box.ships[i].sunk){
 					this.count--;
 					if(this.count <= 0){
-						console.log("Game Over");
+						alert("Game Over");
 					}
 				}
 			}
@@ -59,29 +59,32 @@ var battle_box = {
 
 battle_box.gameover.count =  battle_box.ships.length ;
 
+function color_block(square, color) {
+	return square.style.backgroundColor = color;
+}
+
 // safe distance from side based on 10x10
 function safe_random_number(num1, num2 = 1) {
 	return Math.round(Math.random() * (1 , (num1-1)))+num2;
 }
 
 function computers_turn(){
-	var square = document.getElementById("grid1");
+	var square = document.getElementById("grid0");
 	var x = safe_random_number(10, 0);
 	var y = safe_random_number(10, 0);
-	var block = square.childNodes[x].childNodes[y]
-	var color = "gray";
+	var block = square.childNodes[x].childNodes[y];
 	var active = block.childNodes[2];
+	var color = "gray";
 	
 	if(active.innerHTML == 'true'){
 		for (i in battle_box.ships) {
-			for (j in battle_box.ships[i].cell_computer){
-				if ( x == battle_box.ships[i].cell_computer[j].x && y == battle_box.ships[i].cell_computer[j].y ) {
+			for (j in battle_box.ships[i].cell){
+				if ( x == battle_box.ships[i].cell[j].x && y == battle_box.ships[i].cell[j].y ) {
 					color = "red";
 				} 
-				
 			}
 		}
-		block.style.backgroundColor = color;
+		color_block(block, color)
 		active.innerHTML = "false";
 	} else {
 		// try again		
@@ -94,13 +97,12 @@ var case_test = {
 	test :function (x,y) {
 		var hit = false;
 		for (i in battle_box.ships) {
-			for (j in battle_box.ships[i].cell){
-				if ( x == battle_box.ships[i].cell[j].x && y == battle_box.ships[i].cell[j].y ) {
+			for (j in battle_box.ships[i].cell_computer){
+				if ( x == battle_box.ships[i].cell_computer[j].x && y == battle_box.ships[i].cell_computer[j].y ) {
 					battle_box.ships[i].hit();
 					hit = true;
 				} 
 			}
-			
 		}	
 		computers_turn();	
 		return hit;
@@ -110,15 +112,15 @@ var case_test = {
 // Make GUI grid
 function makeGrid(v){ 
 	var e = document.body; // whatever you want to append the rows to: 
-	for(var j = 1; j <= 2; j++){
+	for(var j = 0; j < 2; j++){
 		var grid = document.createElement("div"); 
 		grid.id = "grid"+j;
     	grid.className = "grid";
 
-		for(var i = 1; i <= v; i++){ 
+		for(var i = 0; i <= v; i++){ 
 	    	var row = document.createElement("div"); 
 	    	row.className = "rows"; 
-	    	for(var x = 1; x <= v; x++){ 
+	    	for(var x = 0; x <= v; x++){ 
 	        	var cell = document.createElement("div"); 
 	        	cell.className = "battleBlock"; 
 	        	cell.innerHTML = "<div class='xAlis axis'>"+i+"</div><div class='yAxis axis'>"+x+"</div><div class='active axis'>true</div>";
@@ -133,6 +135,7 @@ function makeGrid(v){
 	// TO-DO make sure ships dont overlap
 	for(var k = 0; k < 2; k++) {
 		for(i in battle_box.ships){
+			var square = document.getElementById("grid0");
 			var plane = safe_random_number(2,0);
 			var count = battle_box.ships[i].cell_count;
 			var start_number = safe_random_number(10 - count);
@@ -142,24 +145,19 @@ function makeGrid(v){
 			for(j = 1; j <= count; j++){ 
 				// horizontal or vertical
 				if(plane){		
-					if(j == 1){
-						xx = safe_random_number(10);
-					}	
+					if(j == 1){ xx = safe_random_number(10); }	
 					yy = start_number + j;
 				}else{	
-					if(j == 1){
-						yy = safe_random_number(10);
-					}			
+					if(j == 1){ yy = safe_random_number(10); }			
 					xx = start_number + j;						
 				}
 				cell = { x : xx, y : yy };
 
 				if(k){
-					battle_box.ships[i].cell_computer.push(cell);
-					var square = document.getElementById("grid1");
-					square.childNodes[xx].childNodes[yy].style.backgroundColor = "green";
-				} else {
 					battle_box.ships[i].cell.push(cell);
+					color_block(square.childNodes[xx].childNodes[yy], 'green')
+				} else {
+					battle_box.ships[i].cell_computer.push(cell);
 					//console.log("x : "+xx+" - y "+yy);
 				}
 			}
@@ -168,26 +166,20 @@ function makeGrid(v){
 	
 }
 
-
-
 // Game controller
 $(function(){
-	$("body").delegate('#grid2 .battleBlock', 'click', function(ev){
-		var square = $(this);
+	$("body").delegate('#grid1 .battleBlock', 'click', function(ev){
 		var axis = ev.target;
-		var x = axis.childNodes[0].innerHTML;
-		var y = axis.childNodes[1].innerHTML;
 		var active = axis.childNodes[2];
-				
+		var color = 'blue';
+			
 		if(active.innerHTML === 'true' ) {
-			if(case_test.test(x,y)){
-				square.css('background', 'red');				
-			} else {
-				square.css('background', 'blue');
-			}
+			if(case_test.test(axis.childNodes[0].innerHTML,axis.childNodes[1].innerHTML)){
+				color = 'red';
+			} 
+			color_block(axis, color);
 			active.innerHTML = "false";
 		}
-
 	});	
 	
 });
